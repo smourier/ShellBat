afd6bce810e1734aa506f873958639af30f8375a
Simon Mourier <simon_mourier@hotmail.com>
2026-04-13 20:05:43 +0200

    wip

--- ShellN.Extensions/Utilities/FileOpenDialog.cs
+++ ShellN.Extensions/Utilities/FileOpenDialog.cs
@@ -4,4 +4,18 @@ public class FileOpenDialog(nint site = 0, bool enableEvents = false)
     : FileDialog(DirectN.Extensions.Com.ComObject.CoCreate<IFileOpenDialog>(Constants.FileOpenDialog)!, site, enableEvents)
 {
     public static FILEOPENDIALOGOPTIONS DefaultOptions { get; } = FILEOPENDIALOGOPTIONS.FOS_FILEMUSTEXIST | FILEOPENDIALOGOPTIONS.FOS_PATHMUSTEXIST;
+
+    public virtual IReadOnlyList<ShellItem> GetResults()
+    {
+        ((IFileOpenDialog)NativeObject).GetResults(out var array);
+        using var items = new ComObject<IShellItemArray>(array);
+        return [.. items.Enumerate()];
+    }
+
+    public virtual IReadOnlyList<ShellItem> GetSelectedItems()
+    {
+        ((IFileOpenDialog)NativeObject).GetSelectedItems(out var array);
+        using var items = new ComObject<IShellItemArray>(array);
+        return [.. items.Enumerate()];
+    }
 }
